13036a1165c0400b1d3ad6bcfd6679c3b0534023
gokul <142083386+Gokul-Bhatt@users.noreply.github.com>
2024-05-05 11:07:13 +0530

    .

--- src/components/Puja/cards.js
+++ src/components/Puja/cards.js
@@ -2,24 +2,35 @@
 import "./puja.css"
 import { useNavigate } from "react-router-dom";
 
-function PujaCards({title, price, description,image}){
+// function PujaCards({title, price, description,image}){
+    function PujaCards({image,title,price}){
     const navigate = useNavigate();
     return(
-        <div className="cards">
-            <div className="card-image">
-                <figure className="image is-1by1 myimage">
-                    <img src={image} alt="images" />
-                </figure>
-            </div>
-            <div className="card-content">
-                <div className="media-content mythink">
-                    <p className="title is-5 mytitle">{title}</p>
-                    <p className="subtitle is-5 myprice">{price}</p>
-                    <button className="button" onClick={()=> navigate("./register")}>Book</button>
-                </div>
+        // <div className="cards">
+        //     <div className="card-image">
+        //         <figure className="image is-1by1 myimage">
+        //             <img src={image} alt="images" />
+        //         </figure>
+        //     </div>
+        //     <div className="card-content">
+        //         <div className="media-content mythink">
+        //             <p className="title is-5 mytitle">{title}</p>
+        //             <p className="subtitle is-5 myprice">{price}</p>
+        //             <button className="button" onClick={()=> navigate("./register")}>Book</button>
+        //         </div>
                 
-            </div>
-        </div>
+        //     </div>
+        // </div>
+        <div className="w-60 border-2 border-yellow-600 ">
+      <div className="images w-56 h-60">
+        <img className="w-48 h-56" src={image} alt="Image" />
+      </div>
+      <div className="border border-orange-700 bg-gray-100 h-32 ">
+        <p className="text-black font-semibold">{title}</p>
+        <p className="text-black font-semibold">{price}</p>
+        <button className=" text-black font-semibold bg-green-500 border-2 border-green-600 p-2 w-40 rounded-lg hover:bg-green-600 hover:text-white ml-9" onClick={()=> navigate("./register")}>Book</button>
+      </div>
+    </div>
     )
 }
 export default PujaCards;
--- src/components/Puja/puja.js
+++ src/components/Puja/puja.js
@@ -10,60 +10,60 @@ import image4 from "../Aasets/hometwo/homeT4.jpg"
 const puja = () => {
   
   return (
-    <div className='sec'>
-      <section className="hero is-primary">
-        <div className="mytitlemen">
-            <p className="title">
+    <div className=''>
+      <section className="">
+        <div className="">
+            <p className="">
 "जागेश्वर धाम में ये पूजाएं उपलब्ध हैं, पूजा कराने के लिए बुक करें।"</p>
         </div>
       </section>
-      <div className="container">
+      <div className="">
         <section className="section">
             <div className="columns ">
-                <div className="column is-3 pujalist">
-                    <PujaCards title="अष्टाध्यायी रुद्री पाठ" price="@1100" image={image1}/>
+                <div className="">
+                    <PujaCards title="अष्टाध्यायी रुद्री पाठ" price="₹   1100" image={image1}/>
                 </div>
-                <div className="column is-3 pujalist">
-                    <PujaCards title="कालसर्प योग अंक" price="@1100" image={image2}/>
+                <div className="">
+                    <PujaCards title="कालसर्प योग अंक" price="₹   1100" image={image2}/>
                 </div>
-                <div className="column is-3">
-                    <PujaCards title="कालसर्प योग पूर्ण" price="@5100" image={image3}/>
+                <div className="">
+                    <PujaCards title="कालसर्प योग पूर्ण" price="₹   5100" image={image3}/>
                 </div>
-                <div className="column is-3">
-                    <PujaCards title="लक्ष्मी पाठ-108" price="@5100" image={image4}/>
+                <div className="">
+                    <PujaCards title="लक्ष्मी पाठ-108" price="₹   5100" image={image4}/>
                 </div>
-                <div className="column is-3">
-                    <PujaCards title="महामृत्युंजय जाप शनि/राहु/नवगृह-11000" price="@2500" image={image2}/>
+                <div className="">
+                    <PujaCards title="महामृत्युंजय जाप शनि/राहु/नवगृह-11000" price="₹   2500" image={image2}/>
                 </div>
-                <div className="column is-3">
-                    <PujaCards title="महामृत्युंजय जाप शनि/राहु/नवगृह-125000" price="@25000" image={image2}/>
+                <div className="">
+                    <PujaCards title="महामृत्युंजय जाप शनि/राहु/नवगृह-125000" price="₹   25000" image={image2}/>
                 </div>
-                <div className="column is-3">
-                    <PujaCards title="महामृत्युंजय जाप शनि/राहु/नवगृह-23000" price="@5100" image={image2}/>
+                <div className="">
+                    <PujaCards title="महामृत्युंजय जाप शनि/राहु/नवगृह-23000" price="₹   5100" image={image2}/>
                 </div>
-                <div className="column is-3">
-                    <PujaCards title="महामृत्युंजय जाप शनि/राहु/नवगृह-51000" price="@11000" image={image2}/>
+                <div className="">
+                    <PujaCards title="महामृत्युंजय जाप शनि/राहु/नवगृह-51000" price="₹   11000" image={image2}/>
                 </div>
-                <div className="column is-3">
-                    <PujaCards title="पार्थिव पूजा अस्तोतार" price="@550" image={image2}/>
+                <div className="">
+                    <PujaCards title="पार्थिव पूजा अस्तोतार" price="₹   550" image={image2}/>
                 </div>
-                <div className="column is-3">
-                    <PujaCards title="रुद्राभिषेक (सामग्री को छोड़कर)" price="@500" image={image2}/>
+                <div className="">
+                    <PujaCards title="रुद्राभिषेक (सामग्री को छोड़कर)" price="₹   500" image={image2}/>
                 </div>
-                <div className="column is-3">
-                    <PujaCards title="संकल्प पाठ" price="@101" image={image2}/>
+                <div className="">
+                    <PujaCards title="संकल्प पाठ" price="₹   101" image={image2}/>
                 </div>
-                <div className="column is-3">
-                    <PujaCards title="संतान गोपाल जाप-125000" price="@25000" image={image2}/>
+                <div className="">
+                    <PujaCards title="संतान गोपाल जाप-125000" price="₹   25000" image={image2}/>
                 </div>
-                <div className="column is-3">
-                    <PujaCards title="संतान गोपाल जाप-1500" price="@1100" image={image2}/>
+                <div className="">
+                    <PujaCards title="संतान गोपाल जाप-1500" price="₹   1100" image={image2}/>
                 </div>
-                <div className="column is-3">
-                    <PujaCards title="श्री सूक्त लक्ष्मी पाठ-11" price="@500" image={image2}/>
+                <div className="">
+                    <PujaCards title="श्री सूक्त लक्ष्मी पाठ-11" price="₹   500" image={image2}/>
                 </div>
-                <div className="column is-3">
-                    <PujaCards title="श्री सूक्त लक्ष्मी पाठ-51" price="@2100" image={image2}/>
+                <div className="">
+                    <PujaCards title="श्री सूक्त लक्ष्मी पाठ-51" price="₹   2100" image={image2}/>
                 </div>
             </div>
         </section>
