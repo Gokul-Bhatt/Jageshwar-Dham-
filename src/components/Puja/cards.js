17f94df4945bf5bd5f7e5eb91998a5f933efe5e2
gokul <142083386+Gokul-Bhatt@users.noreply.github.com>
2024-05-06 20:15:59 +0530

    .

--- src/components/Puja/cards.js
+++ src/components/Puja/cards.js
@@ -1,26 +1,10 @@
-// import { useState } from "react";
 import "./puja.css";
 import { useNavigate } from "react-router-dom";
 
-// function PujaCards({title, price, description,image}){
+
 function PujaCards({ image, title, price }) {
   const navigate = useNavigate();
   return (
-    // <div className="cards">
-    //     <div className="card-image">
-    //         <figure className="image is-1by1 myimage">
-    //             <img src={image} alt="images" />
-    //         </figure>
-    //     </div>
-    //     <div className="card-content">
-    //         <div className="media-content mythink">
-    //             <p className="title is-5 mytitle">{title}</p>
-    //             <p className="subtitle is-5 myprice">{price}</p>
-    //             <button className="button" onClick={()=> navigate("./register")}>Book</button>
-    //         </div>
-
-    //     </div>
-    // </div>
     <div className="w-60 border-2 border-yellow-600 my-4 mx-4  bg-black ">
       <div className="images w-56 h-60 ">
         <img className="w-48 h-56 mx-5" src={image} alt="Image" />
